8909efa27090ad23b67c0975a07f661e720195eb
88VwV88 <dhimakkvivek773@gmail.com>
2024-02-23 18:15:03 +0530

    Added the map functionality using `bingmaps` API, cleaned up the code and added express.js as the website server.
    Changes to be committed:
    	modified:   LISCENCE.md
    	new file:   app.js
    	modified:   pages/map.html
    	new file:   public/css/map.css
    	new file:   public/css/styles.css
    	new file:   public/images/GDSCLogo.svg
    	new file:   public/images/github.svg
    	new file:   public/images/hackmol.svg
    	new file:   public/images/linkedin.svg
    	new file:   public/js/map.js
    	new file:   public/js/places.json
    	new file:   public/pages/chat.html
    	new file:   public/pages/comp.html
    	new file:   public/pages/index.html
    	new file:   public/pages/map.html
    	modified:   src/map.css
    	modified:   src/map.js
    	new file:   src/places.json
    
    	new file:   package-lock.json
    	new file:   package.json
    	deleted:    pages/map.html
    	new file:   public/css/map.css
    	renamed:    src/styles.css -> public/css/styles.css
    	renamed:    src/assets/GDSCLogo.svg -> public/images/GDSCLogo.svg
    	renamed:    src/assets/github.svg -> public/images/github.svg
    	renamed:    src/assets/hackmol.svg -> public/images/hackmol.svg
    	renamed:    src/assets/linkedin.svg -> public/images/linkedin.svg
    	new file:   public/js/map.js
    	new file:   public/js/places.json
    	renamed:    pages/chatbox.html -> public/pages/chat.html
    	renamed:    pages/complaints.html -> public/pages/comp.html
    	renamed:    index.html -> public/pages/index.html
    	new file:   public/pages/map.html
    	deleted:    src/map.css
    	deleted:    src/map.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan("tiny"));
+
+app.use(express.static('./public'));
+app.use(express.static('./public/pages'));
+
+app.route('/', (_, res) => {
+    res.sendFile('index.html');
+});
+
+app.listen(port, () => {
+    console.log(`HackDesk running on port ${port}`)
+})
